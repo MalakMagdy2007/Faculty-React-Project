b23bf67333a5dc99b6737c151b513fcf74d0f8ac
MalakMagdy2007 <malakmagdy20202007@gmail.com>
2026-08-12 19:33:38 +0000

    fix integration issues

--- src/pages/FacultyDetailPage.jsx
+++ src/pages/FacultyDetailPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { facultyData } from '../data/faculty';
--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { servicesData } from '../data/services';
 import { GraduationCap, Headphones, ExternalLink } from 'lucide-react';
