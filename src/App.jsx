38f49ee9214215ac95e767c13cd58759c7a22143
yousef Elgamily <ey572477@gmail.com>
2026-08-13 14:36:54 +0300

    Refactor App component to include new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,56 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Base & Components
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import LanguageSwitcher from "./components/LanguageSwitcher";
 
+// Person 2 Pages (Departments & Programs)
+import DepartmentsPage from "./pages/DepartmentsPage";
+import DepartmentDetailsPage from "./pages/DepartmentDetailsPage";
+import ProgramsPage from "./pages/ProgramsPage";
+import ProgramDetailsPage from "./pages/ProgramDetailsPage";
+
+// Person 4 Pages (Faculty & Services)
 import { FacultyPage } from "./pages/FacultyPage";
 import { FacultyDetailPage } from "./pages/FacultyDetailPage";
 import { ServicesPage } from "./pages/ServicesPage";
 
-import newsImg1 from './assets/students.jpg'; 
-import newsImg2 from './assets/ai_workshop.jpg'; 
-import newsImg3 from './assets/graduation_project_guidelines.jpg';
+// Person 5 Pages (Events & Contact)
+import Events from "./pages/Events";
+import Contact from "./pages/Contact";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <LanguageSwitcher />
+    return (
+        <BrowserRouter>
+            <LanguageSwitcher />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+            <Routes>
+                {/* Main Routes */}
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
 
-        <Route path="/faculty" element={<FacultyPage />} />
-        <Route path="/faculty/:id" element={<FacultyDetailPage />} />
-        <Route path="/services" element={<ServicesPage />} />
+                {/* Person 2: Departments & Programs */}
+                <Route path="/departments" element={<DepartmentsPage />} />
+                <Route path="/departments/:id" element={<DepartmentDetailsPage />} />
+                <Route path="/programs" element={<ProgramsPage />} />
+                <Route path="/programs/:id" element={<ProgramDetailsPage />} />
 
-        <Route path="/events" element={<Events />} />
-        <Route path="/contact" element={<Contact />} />
+                {/* Person 4: Faculty & Services */}
+                <Route path="/faculty" element={<FacultyPage />} />
+                <Route path="/faculty/:id" element={<FacultyDetailPage />} />
+                <Route path="/services" element={<ServicesPage />} />
 
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/news/:id" element={<NewsDetails />} />
-        <Route path="/announcements" element={<AnnouncementsPage />} />
+                {/* Person 5: Events & Contact */}
+                <Route path="/events" element={<Events />} />
+                <Route path="/contact" element={<Contact />} />
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+                {/* 404 Route */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
